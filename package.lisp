d2c4cbc4eb0318f0e03060e14511f0b9a12465c5
Bruno Dias <dias.h.bruno@gmail.com>
2023-04-24 21:41:35 -0300

    for unix x86-64 we hardcode where to find the library.

--- package.lisp
+++ package.lisp
@@ -6,9 +6,15 @@
 
 (in-package #:cl-unac)
 
-(if (not (or (cffi:foreign-library-loaded-p 'cl-unac.config::libunac)
-	     (cffi:foreign-library-loaded-p 'libunac)))
-    (progn
-      (cffi:define-foreign-library libunac
-	(t (:default "libunac")))
-      (cffi:use-foreign-library libunac)))
+(handler-case
+    (cffi:foreign-library-loaded-p 'cl-unac.config::libunac)
+  (error ()
+    (handler-case
+        (cffi:foreign-library-loaded-p 'libunac)
+      (error ()
+        (progn
+          (cffi:define-foreign-library libunac
+            (:unix (:or #+X86-64 "/usr/lib/x86_64-linux-gnu/libunac.so"
+                        "libunac"))
+            (t (:default "libunac")))
+          (cffi:use-foreign-library libunac))))))
